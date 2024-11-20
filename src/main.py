3b26dbe0e7f1bdef5671f982d4280209864e43d4
Bartlomiej Duda <ikskoks@gmail.com>
2024-11-20 23:11:54 +0100

    v0.4.2

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from reversebox.common.logger import get_logger
 
 from src.GUI.gui_main import ImageHeatGUI
 
-VERSION_NUM = "v0.4.1"
+VERSION_NUM = "v0.4.2"
 
 logger = get_logger("main")
 
---
+++ src/scripts/make_elf.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# TODO
+python setup.py build build_exe
