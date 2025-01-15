347d81e3ed894eaa335a66c05cecc8cee5cd9261
Bartlomiej Duda <ikskoks@gmail.com>
2025-01-15 22:05:04 +0100

    v0.8.5 - performance fix

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from reversebox.common.logger import get_logger
 
 from src.GUI.gui_main import ImageHeatGUI
 
-VERSION_NUM: Final[str] = "v0.8.4"
+VERSION_NUM: Final[str] = "v0.8.5"
 
 logger = get_logger("main")
 
