3c6e7f639d6cd8b394c80b22c8dbd6767fcd0e29
Bartlomiej Duda <ikskoks@gmail.com>
2025-01-19 22:17:35 +0100

    v0.9.2

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from reversebox.common.logger import get_logger
 
 from src.GUI.gui_main import ImageHeatGUI
 
-VERSION_NUM: Final[str] = "v0.9.1"
+VERSION_NUM: Final[str] = "v0.9.2"
 
 logger = get_logger("main")
 
